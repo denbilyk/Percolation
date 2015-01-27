2ac543b0db383fe74de7c31679c1eee7c3550eaa
denis.bilyk <den.bilyk@gmail.com>
2015-01-27 23:26:35 +0200

    Percolate not so work

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,31 +2,55 @@
  * @author denis.bilyk
  */
 public class Percolation {
+    private final byte[] opened;
     private final WeightedQuickUnionUF uf;
     private final int elementsInRow;
+    private final int lastIndex;
 
     public Percolation(int N) {
         if (N <= 0) throw new IllegalArgumentException("N is below zero!");
         elementsInRow = N;
-        int elementsAmount = N*N + 2;
-        int lastIndex = elementsAmount - 1;
+        int elementsAmount = N * N + 2;
+        lastIndex = elementsAmount - 1;
         uf = new WeightedQuickUnionUF(elementsAmount);
+        opened = new byte[elementsAmount];
+        init(elementsAmount);
         createVirtualPoints(0, lastIndex);
     }
 
     public void open(int i, int j) {
         if (i <= 0 || j <= 0) throw new IndexOutOfBoundsException();
         if (!isOpen(i, j)) {
-            uf.union(i, j);
+            int id = getArrayId(i, j);
+            opened[id] = 1;
+            if (i - 1 > 0 && isOpen(i - 1, j)) {
+                uf.union(id, getArrayId(i - 1, j));
+            }
+            if (i + 1 < elementsInRow && isOpen(i + 1, j)) {
+                uf.union(id, getArrayId(i + 1, j));
+            }
+            if (j - 1 > 0 && isOpen(i, j - 1)) {
+                uf.union(id, getArrayId(i, j - 1));
+            }
+            if (j + 1 < elementsInRow && isOpen(i, j + 1)) {
+                uf.union(id, getArrayId(i, j + 1));
+            }
         }
+
     }
 
 
+    int getOpened() {
+        int counter = 0;
+        for (byte b : opened) {
+            if (b == 1) counter++;
+        }
+        return counter;
+    }
+
     // open site (row i, column j) if it is not open already
     public boolean isOpen(int i, int j) {
-
-        // is site (row i, column j) open?
-        return false;
+        return opened[(i - 1) * elementsInRow + (j - 1)] == 1;
     }
 
     public boolean isFull(int i, int j) {
@@ -35,13 +59,27 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return false;
+        return uf.find(lastIndex) == uf.find(0);
+    }
+
+    byte[] getOpenedArray() {
+        return opened;
+    }
+
+    private int getArrayId(int i, int j) {
+        return (i - 1) * elementsInRow + (j - 1) + 1;
     }
 
     private void createVirtualPoints(int topPoint, int bottomPoint) {
         for (int i = 0, j = bottomPoint - 1; i < elementsInRow; i++, j--) {
-            uf.union(topPoint, i+1);
+            uf.union(topPoint, i + 1);
             uf.union(bottomPoint, j);
+        }
+    }
+
+    private void init(int number) {
+        for (int i = 0; i < number; i++) {
+            opened[i] = 0;
         }
     }
 
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -1,22 +1,40 @@
 import org.junit.*;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class PercolationTest {
+    private Random rnd = new Random();
 
 
     @Test
     public void test() {
         int rowSize = 5;
         Percolation percolation = new Percolation(rowSize);
+        while (!percolation.percolates()) {
+            int valueR = rnd.nextInt(rowSize) + 1;
+            int valueC = rnd.nextInt(rowSize) + 1;
+            percolation.open(valueR, valueC);
+            //System.out.println("Row: " + valueR + "  ==  Col: " + valueC);
+            //print(percolation.getOpenedArray(), rowSize);
+            //System.out.println();
+        }
+
+        System.out.println("Results: ");
+        print(percolation.getOpenedArray(), rowSize);
+        System.out.println();
+        System.out.println("Count: " + percolation.getOpened());
+        System.out.println("Thresholds: " + (double)percolation.getOpened()/(rowSize * rowSize));
+
     }
 
 
-    private void print(int[] array, int rowSize) {
-        for (int j = 0; j < rowSize; j++) {
-            for (int i = 0; i < rowSize; i++) {
-                System.out.print(array[rowSize*j + i]);
+    private void print(byte[] array, int rowSize) {
+        for (int j = 1; j <= rowSize; j++) {
+            for (int i = 1; i <= rowSize; i++) {
+                System.out.print(array[rowSize * (j - 1) + (i - 1)]);
                 System.out.print(" ");
             }
             System.out.println();
